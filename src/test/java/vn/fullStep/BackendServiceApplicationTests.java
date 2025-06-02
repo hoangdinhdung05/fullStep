daf7830368ff295e53d6023e2520f3f381f8cdb7
hoangdinhdung05 <hoangdinhdung0205@gmail.com>
2025-06-03 00:49:16 +0700

    cấu hình các file, tạo base chuẩn trong dự án

---
+++ src/test/java/vn/fullStep/BackendServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package vn.fullStep;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
